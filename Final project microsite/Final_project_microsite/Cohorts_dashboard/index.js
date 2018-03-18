2cc0f391d8e7723b6b05d31c9cb5be3c4f054df5
SabeenK8 <35353661+SabeenK8@users.noreply.github.com>
2018-03-18 16:24:07 -0400

    Added .js files with pseudocode
    Renamed folders and reorganized, .js files have psuedocode, some HTML corrections

---
+++ Final project microsite/Final_project_microsite/Cohorts_dashboard/index.js
@@ -0,0 +1,16 @@
+// jQuery(document).ready(function(){
+//     jQuery('#').click(function (e) {
+//         e.preventDefault()
+//         jQuery().tab('show')
+//     })
+
+// Function to insert name if user is signed in
+
+// Function to log out if user is signed in
+
+// Function to show any appended replies in Replies list item for Mobile medical apps thread
+
+// Function to notify users that search didn't find anything
+
+
+// });
---
+++ Final project microsite/Final_project_microsite/Landing_page/index.js
@@ -0,0 +1,29 @@
+// jQuery(document).ready(function(){
+//     jQuery('#').click(function (e) {
+//         e.preventDefault()
+//         jQuery().tab('show')
+//     })
+
+// Function to insert name if user is signed in
+
+// Function to log out if user is signed in
+
+// Function to log completed forms
+
+// Function to only accept email inputs with an "@" symbol
+
+// Function to only accept full names with two words separate by a space
+
+// Function to check password confirmation against first password input
+
+// Function to alert users they must accept the terms of use
+
+// Function to notify users who forgot password that a link to reset their password has been sent to their email
+
+// Function to show a lightbox to write a post for social sharing: Twitter
+
+// Function to show a lightbox to write a post for social sharing: Facebook
+
+// Function to show a lightbox to write a post for social sharing: LinkedIn
+    
+// });
